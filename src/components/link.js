9a0dbded6519d68bade4390e3fdc53b10306c39b
mkotsollaris <mkotsollari@gmail.com>
2019-11-11 16:13:28 -0800

    fixed Linked component

--- src/components/link.js
+++ src/components/link.js
@@ -2,10 +2,21 @@ import React from 'react'
 import GatsbyLink from 'gatsby-link'
 
 const Link = ({children, to, ...other}) => {
-     return  <GatsbyLink to={to} {...other}>
+  const internal = /^\/(?!\/)/.test(to)
+
+  if (internal) {
+    return (
+      <GatsbyLink to={to} {...other}>
         {children}
       </GatsbyLink>
-   
+    )
+  }
+
+  return (
+    <a href={to} {...other}>
+      {children}
+    </a>
+  )
 }
 
 export default Link
