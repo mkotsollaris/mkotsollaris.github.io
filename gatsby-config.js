79d56c976ed96d4e161b9951ca6c9534cb05f65a
mkotsollaris <mkotsollari@gmail.com>
2019-11-17 12:18:01 -0800

    google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,18 +14,6 @@ module.exports = {
       options: {
         trackingId: "UA-152012397-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
       },
     },
     {
